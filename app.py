9e90ac683d76fe4ab9ba1441c2e773a796c66f73
Jose Ramon Macias <jrmacias@ebi.ac.uk>
2017-08-10 16:15:26 +0100

    Add separate modes for testing and downloading
    A few options have been added to the command line allowing for a test mode, where the input JSON file is read, parsed and its content printed back. In download mode, the JSON input file is parsed and all the associated files are download from the METASPACE AWS cloud repository.

--- app.py
+++ app.py
@@ -14,36 +14,66 @@ logger = logging.getLogger(__name__)
 
 
 def main(argv):
-    options = 'hi:o:'
-    options_help = '[-h, -i <inputfile.json>] -o <outputdir>]'
+    options = 'htdi:o:'
+    options_help = """ [-h, -t, -i <inputfile.json>] -o <outputdir>]
+                   
+                   -h   --help          Display this message.
+                   -i   --inputfile     Provide the JSON input file.
+                   -o   --outputdir     Set the output folder. Will be created if not found.
+                   -d   --download      Download METASPACE study associated files. 
+                   -t   --testmode      Read the input JSON file provided with option -i and print its content.
+                   """
 
     input_file = ''
     output_dir = ''
+    test_mode = False
+    download_mode = False
 
     try:
-        opts, args = getopt.getopt(argv, options, ['inputfile=', 'outputdir='])
+        opts, args = getopt.getopt(argv, options, ['help', 'test', 'download', 'inputfile=', 'outputdir='])
     except getopt.GetoptError:
         print('Use: python ' + os.path.basename(sys.argv[0]) + options_help)
         sys.exit(2)
     for opt, arg in opts:
-        if opt == '-h':
+        if opt in ('-h', '--help'):
             print('Read METASPACE JSON export file')
             print('Use: python ' + os.path.basename(sys.argv[0]) + options_help)
             sys.exit()
         if opt in ('-i', '--inputfile'):
             input_file = arg
-        if opt in ('-o', '--output'):
+        if opt in ('-o', '--outputdir'):
             output_dir = arg
+        if opt in ('-d', '--download'):
+            download_mode = True
+        if opt in ('-t', '--test'):
+            test_mode = True
 
+    mtspc_obj = parse(input_file)
+
+    if test_mode:
+        print_mtspc_obj(mtspc_obj)
+        exit(0)
+
+    if download_mode:
+        aws_download_files(mtspc_obj, output_dir)
+        exit(0)
+
+
+def print_mtspc_obj(mtspc_obj):
+    for sample in mtspc_obj:
+        for key, value in sample.items():
+            print(key, value)
+        print()
+
+
+def aws_download_files(mtspc_obj, output_dir):
     credentials = configparser.ConfigParser()
     credentials.read(config.AWS_CREDENTIALS)
     mtspc = credentials['METASPACE']
-
     aws_bucket = mtspc['bucket'] + '/'
 
-    submissions = read_josn_file(input_file)
-    for submission in submissions:
-        imzml_file = get_imzml_filename(submission).replace(aws_bucket, '')
+    for sample in mtspc_obj:
+        imzml_file = get_imzml_filename(sample).replace(aws_bucket, '')
         path = imzml_file.split('/')
         folder = path[0]
         filename = path[1]
@@ -53,7 +83,7 @@ def main(argv):
         if imzml is not None:
             save_file(imzml, output_dir, filename)
 
-        ibd_file = get_ibd_filename(submission).replace(aws_bucket, '')
+        ibd_file = get_ibd_filename(sample).replace(aws_bucket, '')
         path = ibd_file.split('/')
         folder = path[0]
         filename = path[1]
@@ -64,21 +94,22 @@ def main(argv):
             save_file(ibd, output_dir, filename, data_type='binary')
 
 
-def read_josn_file(file_name):
-    with open(file_name) as data_file:
+def parse(filename):
+    assert os.path.exists(filename), "Did not find json input file: %s" % filename
+    with open(filename, 'r', encoding='utf-8') as data_file:
         json_data = json.load(data_file)
     return json_data
 
 
-def get_imzml_filename(submission):
+def get_imzml_filename(sample_data):
 
-    submitted_By = submission['Submitted_By']
-    metaspace_options = submission['metaspace_options']
-    Sample_Information = submission['Sample_Information']
-    Additional_Information = submission['Additional_Information']
-    MS_Analysis = submission['MS_Analysis']
-    Sample_Preparation = submission['Sample_Preparation']
-    s3dir = submission['s3dir']
+    submitted_By = sample_data['Submitted_By']
+    metaspace_options = sample_data['metaspace_options']
+    Sample_Information = sample_data['Sample_Information']
+    Additional_Information = sample_data['Additional_Information']
+    MS_Analysis = sample_data['MS_Analysis']
+    Sample_Preparation = sample_data['Sample_Preparation']
+    s3dir = sample_data['s3dir']
 
     filename = metaspace_options['Dataset_Name'] + '.imzML'
     path = os.path.join(s3dir['imzML'].strip(filename), filename)
