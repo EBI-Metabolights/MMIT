9d967c61632bb4ab7793d61a96714c6527ae7f55
Jose Ramon Macias <jrmacias@ebi.ac.uk>
2017-09-06 15:38:41 +0100

    Refactor code to separate downloads of different types of files.
    To download different file tipes separately, the file extension can be provided as a command line parameter.

--- mmit.py
+++ mmit.py
@@ -20,18 +20,20 @@ def main(argv):
                    -h   --help          Display this message.
                    -v   --version       Display version information.
                    -i   --inputfile     Provide the JSON input file.
-                   -o   --outputdir     Set the output folder. Will be created if not found.
-                   -d   --download      Download METASPACE study associated files. 
+                   -o   --outputdir     Set the output folder. Will be created if not found. 
                    -t   --testmode      Read the input JSON file provided with option -i and print its content.
+                        --imzml         Download *.imzml study associated files.
+                        --ibd           Download *.ibd study associated files.
                    """
 
     input_file = ''
     output_dir = ''
     test_mode = False
-    download_mode = False
+    download_imzml = False
+    download_ibd = False
 
     try:
-        opts, args = getopt.getopt(argv, options, ['help', 'version', 'test', 'download', 'inputfile=', 'outputdir='])
+        opts, args = getopt.getopt(argv, options, ['help', 'version', 'test', 'imzML', 'ibd', 'inputfile=', 'outputdir='])
     except getopt.GetoptError:
         print('Use: python ' + os.path.basename(sys.argv[0]) + options_help)
         sys.exit(2)
@@ -52,10 +54,12 @@ def main(argv):
             input_file = arg
         if opt in ('-o', '--outputdir'):
             output_dir = arg
-        if opt in ('-d', '--download'):
-            download_mode = True
         if opt in ('-t', '--test'):
             test_mode = True
+        if opt == '--imzML':
+            download_imzml = True
+        if opt == '--ibd':
+            download_ibd = True
 
     mtspc_obj = parse(input_file)
 
@@ -63,9 +67,10 @@ def main(argv):
         print_mtspc_obj(mtspc_obj)
         exit(0)
 
-    if download_mode:
-        aws_download_files(mtspc_obj, output_dir)
-        exit(0)
+    if download_imzml:
+        aws_download_files(mtspc_obj, output_dir, 'imzML', data_type='utf-8')
+    if download_ibd:
+        aws_download_files(mtspc_obj, output_dir, 'ibd', data_type='binary')
 
 
 def print_mtspc_obj(mtspc_obj):
@@ -75,32 +80,41 @@ def print_mtspc_obj(mtspc_obj):
         print()
 
 
-def aws_download_files(mtspc_obj, output_dir):
+def aws_download_files(mtspc_obj, output_dir, extension, data_type='binary'):
     credentials = configparser.ConfigParser()
     credentials.read(config.AWS_CREDENTIALS)
     mtspc = credentials['METASPACE']
     aws_bucket = mtspc['bucket'] + '/'
 
     for sample in mtspc_obj:
-        imzml_file = get_imzml_filename(sample).replace(aws_bucket, '')
-        path = imzml_file.split('/')
+        a_file = get_filename(sample, extension).replace(aws_bucket, '')
+        path = a_file.split('/')
         folder = path[0]
         filename = path[1]
-        logger.info("Getting file %s", imzml_file)
+        logger.info("Getting file %s", a_file)
+        file = aws_download_file(os.path.join(folder, filename), data_type)
+        if file is not None:
+            save_file(file, output_dir, filename, data_type)
 
-        imzml = aws_download_file(os.path.join(folder, filename), 'utf-8')
-        if imzml is not None:
-            save_file(imzml, output_dir, filename)
+        # imzml_file = get_imzml_filename(sample).replace(aws_bucket, '')
+        # path = imzml_file.split('/')
+        # folder = path[0]
+        # filename = path[1]
+        # logger.info("Getting file %s", imzml_file)
+        #
+        # imzml = aws_download_file(os.path.join(folder, filename), 'utf-8')
+        # if imzml is not None:
+        #     save_file(imzml, output_dir, filename)
 
-        ibd_file = get_ibd_filename(sample).replace(aws_bucket, '')
-        path = ibd_file.split('/')
-        folder = path[0]
-        filename = path[1]
-        logger.info("Getting file %s", ibd_file)
-
-        ibd = aws_download_file(os.path.join(folder, filename))
-        if ibd is not None:
-            save_file(ibd, output_dir, filename, data_type='binary')
+        # ibd_file = get_ibd_filename(sample).replace(aws_bucket, '')
+        # path = ibd_file.split('/')
+        # folder = path[0]
+        # filename = path[1]
+        # logger.info("Getting file %s", ibd_file)
+        #
+        # ibd = aws_download_file(os.path.join(folder, filename))
+        # if ibd is not None:
+        #     save_file(ibd, output_dir, filename, data_type='binary')
 
 
 def parse(filename):
@@ -110,38 +124,33 @@ def parse(filename):
     return json_data
 
 
-def get_imzml_filename(sample_data):
-
-    submitted_By = sample_data['Submitted_By']
+def get_filename(sample_data, extension):
     metaspace_options = sample_data['metaspace_options']
-    Sample_Information = sample_data['Sample_Information']
-    Additional_Information = sample_data['Additional_Information']
-    MS_Analysis = sample_data['MS_Analysis']
-    Sample_Preparation = sample_data['Sample_Preparation']
     s3dir = sample_data['s3dir']
-
-    filename = metaspace_options['Dataset_Name'] + '.imzML'
-    path = os.path.join(s3dir['imzML'].strip(filename), filename)
-    # print(metaspace_options['Dataset_Name'], path)
-
+    filename = metaspace_options['Dataset_Name'] + '.' + extension
+    path = os.path.join(s3dir[extension].strip(filename), filename)
     return path
 
 
-def get_ibd_filename(submission):
-    metaspace_options = submission['metaspace_options']
-    s3dir = submission['s3dir']
+# def get_imzml_filename(sample_data):
+#     metaspace_options = sample_data['metaspace_options']
+#     s3dir = sample_data['s3dir']
+#     filename = metaspace_options['Dataset_Name'] + '.imzML'
+#     path = os.path.join(s3dir['imzML'].strip(filename), filename)
+#     return path
 
-    filename = metaspace_options['Dataset_Name'] + '.ibd'
-    path = os.path.join(s3dir['ibd'].strip(filename), filename)
 
-    return path
+# def get_ibd_filename(submission):
+#     metaspace_options = submission['metaspace_options']
+#     s3dir = submission['s3dir']
+#     filename = metaspace_options['Dataset_Name'] + '.ibd'
+#     path = os.path.join(s3dir['ibd'].strip(filename), filename)
+#     return path
 
 
 def save_file(content, path, filename, data_type='text'):
-
     if not os.path.exists(path):
         os.makedirs(path)
-
     mode = 'w'
     if data_type == 'binary':
         mode = 'wb'
